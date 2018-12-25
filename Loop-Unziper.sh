a426cf3d10c7b2b6202652207d8ba145fe55f0cd
Mr-Aleph-Null <46067811+Mr-Aleph-Null@users.noreply.github.com>
2018-12-26 01:10:50 +0800

    Update Loop-Unziper.sh

--- Loop-Unziper.sh
+++ Loop-Unziper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Make sure that you have placed the Wordlist and ZIP file in the same directory as the script file. Note that the brute force option will just be numeric and very simple"
-echo "Do you want to brute-force? 'No' for wordlits [Y,N]"
+echo "Do you want to brute-force? 'No' for wordlits, 'F' for the password being the file name [Y , N, F]"
 read input
 if [[ $input == "Y" || $input == "y" ]]; then
         echo "What is the file name?"
@@ -16,10 +16,23 @@ if [[ $input == "Y" || $input == "y" ]]; then
               zipfile="$next_zipfile"
      
     done
+elif [[ $input == "F" || $input == "f" ]]; then
+         echo "What is the file name?"
+         read file
+         pw=$(zipinfo -1 "$file" | cut -d. -f1)
+         unzip -P "$pw" "$file"
+         zipfile="$file"
+    
+         while unzip -Z1 "$zipfile" | head -n1 | grep "\.zip$"; do
+              next_zipfile="$(unzip -Z1 "$zipfile" | head -n1)"
+              unzip -P "${next_zipfile%.*}" "$zipfile"
+              zipfile="$next_zipfile"
+    done
+
 else
         echo "What is the file name?"
         read file
-        echo "Incert the word list name (e.g rockyou.txt)"
+        echo "insert the wordlist location"
         read location
 
         zipfile="$file"
